2feec50d946f6b47062241cf80bb63f282ec2916
theCode <CODY.WEST7137@STU.MTEC.EDU>
2022-08-28 20:01:38 -0600

    complete

---
+++ app.js
@@ -0,0 +1,10 @@
+$(".navItem").each(function(index){
+    $(this).css('color','white');
+  })
+
+$('#nav').css('background-color','#145')
+
+$('.even').css('background-color','#ccc')
+$('.odd').css('background-color','#aaa')
+
+$('input').val('Cody')
